d7332ecb814f92630133480313d7048f22db24ce
Gaglo Koudjo Espoir <espoirgaglo@gmail.com>
2022-12-09 16:20:26 -0800

    print number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Print all numbers from base 10
+ *
+ * Return: always 0 if success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+
+}

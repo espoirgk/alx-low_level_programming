50d17ecaa3bb6528a20b45e4a67acb69e9df2b0b
Gaglo Koudjo Espoir <espoirgaglo@gmail.com>
2022-12-15 17:19:43 -0800

    most number to print

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers
+ * Return:0
+ */
+
+void print_most_numbers(void)
+{
+	char i = 0;
+
+	while (i <= 9)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar('0' + i);
+		}
+
+		i++;
+	}
+	_putchar('\n');
+
+}

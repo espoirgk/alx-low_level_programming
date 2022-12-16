e8baf95287cb7a2a188bd29705e5127df83352d5
Gaglo Koudjo Espoir <espoirgaglo@gmail.com>
2022-12-15 17:16:21 -0800

    upper number

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if the caracter is a uppercase
+ * @c: caracter to checl
+ * Return: 1 if is upper 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	return (c >= 65 && c <= 90);
+
+}

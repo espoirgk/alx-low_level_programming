22b35cdcc1938dd7dd0ab49b01d31ebfa5060437
Gaglo Koudjo Espoir <espoirgaglo@gmail.com>
2022-12-15 17:18:05 -0800

    numbera X numberb

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiplies integers
+ * @a: integer to multiply to b
+ * @b: integer to multiply to a
+ * Return: the result of the multiplication
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}

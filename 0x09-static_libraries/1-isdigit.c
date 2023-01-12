75cdf42b011fb4265d80896f7ddec64f199fc136
Gaglo Koudjo Espoir <espoirgaglo@gmail.com>
2023-01-11 16:53:16 -0800

    library

---
+++ 0x09-static_libraries/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit
+ * @c: the number checked
+ * Return: 1 if c is a digit 0 otherwise
+ */
+int _isdigit(int c)
+{
+	return (c >= 48 && c <= 57);
+}
---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower - checks wether the caracter is lower or not
+ * @c: represent the caracter to test
+ * Return: 0 or 1
+ */
+
+int _islower(int c)
+{
+	return (c >= 97 && c <= 122);
+}

27f367fda84f00c60ad36f8db79c50b5255bfad4
Espoir Gaglo <espoirgaglo@gmail.com>
2023-01-29 15:33:04 -0800

    function that executes a function given as a parameter on each element of an array

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,55 @@
+#include "3-calc.h"
+/**
+ * op_add - addition of two integers
+ * @a: integer1
+ * @b: integer2
+ * Return: sum a, b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - substract
+ * @a: integer1
+ * @b: integer2
+ * Return: a - b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplication
+ * @a: integer1
+ * @b: integer2
+ * Return: a * b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - division
+ * @a: integer1
+ * @b: integer2
+ * Return: a / b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulo
+ * @a: integer1
+ * @b: integer2
+ * Return: a % b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}

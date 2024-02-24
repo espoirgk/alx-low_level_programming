2267aba5ad5a804e705a1bfb78ed7ec8b485d287
Espoir Gaglo <58880576+espoirgk@users.noreply.github.com>
2024-02-24 01:45:01 +0000

    update function

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /*
- *_puts_recursion is a function that prints a string follow by new line
+ *_puts_recursion - a function that prints a string follow by new line
  *@s: argument to be printed
  */
 void _puts_recursion(char *s)

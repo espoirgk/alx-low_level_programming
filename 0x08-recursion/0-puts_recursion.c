9f7a57fbb3973fd353a82074f5ae6fb8f750fe23
espoirgk <espoirgaglo@gmail.com>
2024-02-23 17:14:29 -0800

    recursive function

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /*
- * _puts_recursion: functin that prints a string follow by new line
- * s: argument to be printed
+ * _puts_recursion: a function that prints a string follow by new line
+ * @s: argument to be printed
  */
 void _puts_recursion(char *s)
 {
 	if (*s != '\0')
 	{
 		_putchar(*s);
-		_puts_recursion(s+1);
+		_puts_recursion(s + 1);
 	}
 	else
 		_putchar('\n');
